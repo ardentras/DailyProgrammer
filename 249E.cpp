fdac7ece87b1eb5d80e55f5e3447bfb3fcfb52d8
Shaun Rasmusen <shaunrasmusen@gmail.com>
2016-01-11 20:46:18 -0800

    Simplified and made 249E prettier

--- 249E.cpp
+++ 249E.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-using std::cout;
 
 int main()
 {
-	float lowTrade = 0.0F;
-	float buyTrade = 0.0F;
-	float highTrade = 0.0F;
-	float currTrade = 0.0F;
 	float stocks[] = {  9.20, 8.03, 10.02, 8.08, 8.14, 8.10, 8.31, 8.28, 8.35, 8.34, 8.39, 8.45,
 						8.38, 8.38, 8.32, 8.36, 8.28, 8.28, 8.38, 8.48, 8.49, 8.54, 8.73, 8.72, 
 						8.76, 8.74, 8.87, 8.82, 8.81, 8.82, 8.85, 8.85, 8.86, 8.63, 8.70, 8.68, 
@@ -16,13 +11,21 @@ int main()
 						8.82, 8.83, 8.91, 8.80, 8.97, 8.86, 8.81, 8.87, 8.82, 8.78, 8.82, 8.77, 
 						8.54, 8.32, 8.33, 8.32, 8.51, 8.53, 8.52, 8.41, 8.55, 8.31, 8.38, 8.34, 
 						8.34, 8.19, 8.17, 8.16 };
+	float lowTrade = stocks[0];
+	float highTrade = stocks[0];
 
 	for (int i = 0; i < sizeof(stocks)/sizeof(float); i++)
 	{
-
+		if (lowTrade > stocks[i])
+		{
+			lowTrade = stocks[i];
+			i += 2;
+		}
+		if (highTrade < stocks[i])
+			highTrade = stocks[i];
 	}
 
-	cout << lowTrade << " " << highTrade << "\n";
+	std::cout << lowTrade << " " << highTrade << "\n";
 
 	return 0;
 }
